9798c26286c4c4305b9f8a08370615b6cd3ab904
ruko <rukokarasu@gmail.com>
2022-05-18 01:08:55 -0700

    wip

--- frontend/src/Main.elm
+++ frontend/src/Main.elm
@@ -1,22 +1,50 @@
-module Main exposing (..)
+port module Main exposing (..)
 
 import Browser
 import Html exposing (Html, button, div, text)
 import Html.Events exposing (onClick)
 
-main = Browser.sandbox { init = 0, update = update, view = view }
+main = Browser.element
+  { init = init
+  , update = update
+  , view = view
+  , subscriptions = subscriptions
+  }
+
+port getModel : String -> Cmd msg
+
+type alias Model = 
+  { number : Int
+  }
+
+init : () -> (Model, Cmd Msg)
+init flags =
+  ( { number = 0 }
+  , Cmd.none
+  )
 
 type Msg = Increment | Decrement
 
+update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
-    Increment -> model + 1
-    Decrement -> model - 1
+    Increment ->
+      ( { model | number = model.number + 1 }
+      , getModel "inc"
+      )
+    Decrement ->
+      ( { model | number = model.number - 1 }
+      , getModel "dec"
+      )
 
+subscriptions : Model -> Sub Msg
+subscriptions _ = Sub.none
+
+view : Model -> Html Msg
 view model =
   div
     []
     [ button [onClick Decrement] [text "-"]
-    , div [] [text (String.fromInt model)]
+    , div [] [text (String.fromInt model.number)]
     , button [onClick Increment] [text "+"]
     ]
--- src/main.rs
+++ src/main.rs
@@ -19,6 +19,20 @@ async fn js() -> Result<NamedFile> {
   Ok(named_file)
 }
 
+#[get("/vendor/three.js")]
+async fn three() -> Result<NamedFile> {
+  let path = Path::new("frontend").join("vendor").join("three.js");
+  let named_file = NamedFile::open(path).await?;
+  Ok(named_file)
+}
+
+#[get("/vendor/GLTFLoader.js")]
+async fn gltf_loader() -> Result<NamedFile> {
+  let path = Path::new("frontend").join("vendor").join("GLTFLoader.js");
+  let named_file = NamedFile::open(path).await?;
+  Ok(named_file)
+}
+
 #[get("/model/<model_id>")]
 async fn get_model(model_id: &str) -> Result<(ContentType, NamedFile)> {
   let path = Path::new("digimons").join(model_id).join("out.gltf");
@@ -29,5 +43,5 @@ async fn get_model(model_id: &str) -> Result<(ContentType, NamedFile)> {
 
 #[launch]
 fn rocket() -> _ {
-  rocket::build().mount("/", routes![index, js, get_model])
+  rocket::build().mount("/", routes![index, js, three, gltf_loader, get_model])
 }
