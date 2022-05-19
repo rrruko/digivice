179c7c5d1b221bdfa158735738d1edcbc8beaa3b
ruko <rukokarasu@gmail.com>
2022-05-19 00:57:32 -0700

    enumerate all model files on startup

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,68 @@
 #[macro_use] extern crate rocket;
 
-use std::io::Result;
+use std::ffi::OsString;
+use std::result::Result;
+use std::io::Result as IOResult;
 use std::path::Path;
 use rocket::http::ContentType;
 use rocket::fs::NamedFile;
+use std::fs;
 
 #[get("/")]
-async fn index() -> Result<NamedFile> {
+async fn index() -> IOResult<NamedFile> {
   let path = Path::new("frontend").join("index.html");
   let named_file = NamedFile::open(path).await?;
   Ok(named_file)
 }
 
 #[get("/main.js")]
-async fn js() -> Result<NamedFile> {
+async fn js() -> IOResult<NamedFile> {
   let path = Path::new("frontend").join("main.js");
   let named_file = NamedFile::open(path).await?;
   Ok(named_file)
 }
 
 #[get("/vendor/three.js")]
-async fn three() -> Result<NamedFile> {
+async fn three() -> IOResult<NamedFile> {
   let path = Path::new("frontend").join("vendor").join("three.js");
   let named_file = NamedFile::open(path).await?;
   Ok(named_file)
 }
 
 #[get("/vendor/GLTFLoader.js")]
-async fn gltf_loader() -> Result<NamedFile> {
+async fn gltf_loader() -> IOResult<NamedFile> {
   let path = Path::new("frontend").join("vendor").join("GLTFLoader.js");
   let named_file = NamedFile::open(path).await?;
   Ok(named_file)
 }
 
 #[get("/model/<model_id>")]
-async fn get_model(model_id: &str) -> Result<(ContentType, NamedFile)> {
+async fn get_model(model_id: &str) -> IOResult<(ContentType, NamedFile)> {
   let path = Path::new("digimons").join(model_id).join("out.gltf");
   let custom = ContentType::new("model", "gtlf+json");
   let named_file = NamedFile::open(path).await?;
   Ok((custom, named_file))
 }
 
+fn list_digimons<'a>() -> Result<Vec<String>, String> {
+  let read_dir = fs::read_dir(Path::new("digimons"))
+    .map_err(|e| format!("{:?}", e))?;
+  read_dir
+    .map(|x| {
+       match x {
+         Ok(entry) => entry
+           .file_name()
+           .into_string()
+           .map_err(|_| String::from("Uh oh")),
+         Err(bad) => Err(format!("{:?}", bad))
+       }
+    })
+    .collect()
+}
+
 #[launch]
 fn rocket() -> _ {
-  rocket::build().mount("/", routes![index, js, three, gltf_loader, get_model])
+  list_digimons();
+  rocket::build()
+    .mount("/", routes![index, js, three, gltf_loader, get_model])
 }
