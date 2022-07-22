a3626d497561d1423effd624f6521297ccd5ad3c
ruko <rukokarasu@gmail.com>
2022-07-21 19:17:40 -0700

    flake

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    naersk.url = "github:nix-community/naersk";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, naersk }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages."${system}";
+        naersk-lib = naersk.lib."${system}";
+      in
+        rec {
+          # `nix build`
+          packages.digivice = naersk-lib.buildPackage {
+            pname = "digivice";
+            root = ./.;
+          };
+          defaultPackage = packages.digivice;
+
+          packages.frontend = pkgs.stdenv.mkDerivation {
+            name = "frontend";
+            src = ./frontend;
+            installPhase = ''
+              cp -r $src $out
+            '';
+          };
+
+          # `nix run`
+          apps.digivice = flake-utils.lib.mkApp {
+            drv = packages.digivice;
+          };
+          defaultApp = apps.digivice;
+
+          # `nix develop`
+          devShell = pkgs.mkShell {
+            nativeBuildInputs = with pkgs; [ rustc cargo elmPackages.elm ];
+          };
+        }
+    );
+}
